3d72f6a7dce4eab0e739dcb9cf9fa6eed9bda36b
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-04-25 14:00:25 +0300

    add sample android test

--- src/test/java/tests/sample/TestBase.java
+++ src/test/java/tests/sample/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
     public static void setup(){
         Configuration.browser = BrowserstackMobileDriver.class.getName();
         Configuration.startMaximized = false;
+        Configuration.browserSize = null;
 
     }
 
