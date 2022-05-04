439d8620fb48f18ec44b60013ffe7c0a81cea310
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-05-04 17:31:46 +0300

    new branch android with EmulatorMobileDriver

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -3,42 +3,40 @@ package drivers;
 import com.codeborne.selenide.WebDriverProvider;
 import config.Credentials;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BrowserstackMobileDriver implements WebDriverProvider {
 
+    @Override
+    public WebDriver createDriver(Capabilities capabilities) {
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        mutableCapabilities.merge(capabilities);
+        mutableCapabilities.setCapability("browserstack.appium_version", "1.22.0");
+        mutableCapabilities.setCapability("browserstack.user", Credentials.config.user());
+        mutableCapabilities.setCapability("browserstack.key", Credentials.config.key());
+        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        mutableCapabilities.setCapability("device", "Google Pixel 3");
+        mutableCapabilities.setCapability("os_version", "9.0");
+        mutableCapabilities.setCapability("project", "QA.GURU lesson 11/22");
+        mutableCapabilities.setCapability("build", "browserstack-build-1");
+        mutableCapabilities.setCapability("name", "Sample tests run");
+
+        return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
+    }
+
     public static URL getBrowserstackUrl() {
         try {
-            return new URL(Credentials.config.url());
+            return new URL("http://hub.browserstack.com/wd/hub");
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    @Override
-    public WebDriver createDriver(DesiredCapabilities caps) {
-
-        // Set your access credentials
-        caps.setCapability("browserstack.user", Credentials.config.user());
-        caps.setCapability("browserstack.key", Credentials.config.key());
-
-        // Set URL of the application under test
-        caps.setCapability("app", Credentials.config.app());
-
-        // Specify device and os_version for testing
-        caps.setCapability("device", "Google Pixel 3");
-        caps.setCapability("os_version", "9.0");
-
-        // Set other BrowserStack capabilities
-        caps.setCapability("project", "First Java Project");
-        caps.setCapability("build", "browserstack-build-1");
-        caps.setCapability("name", "first_test");
-
-
-        return new AndroidDriver(getBrowserstackUrl(), caps);
-    }
 }
